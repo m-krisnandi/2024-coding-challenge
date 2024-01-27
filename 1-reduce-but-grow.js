9bd08f6a3484a9dce917b71780ffd1f251ac5fe5
Muhamad Krisnandi <mkrisnandi08@gmail.com>
2024-01-28 06:47:52 +0700

    challenge 1/366

--- 1-reduce-but-grow.js
+++ 1-reduce-but-grow.js
@@ -1,3 +1,7 @@
+// Coding Challenge 2024
+// 1/366
+// https://www.codewars.com/kata/57f780909f7e8e3183000078
+
 // 1 Solution
 // const grow = (x) => x.reduce((acc, curr) => acc * curr);
 
@@ -5,10 +9,20 @@
 // const grow=x=>eval(x.join("*"))
 
 // 3 Solution
+// const grow = x => {
+//     let res = 1;
+//     for (let i = 0; i < x.length; i++) {
+//         res *= x[i]
+//     }
+
+//     return res
+// }
+
+// 4 Solution
 const grow = x => {
     let res = 1;
-    for (let i = 0; i < x.length; i++) {
-        res *= x[i]
+    for (const element of x) {
+        res *= element
     }
 
     return res
