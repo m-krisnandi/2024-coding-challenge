af3d7c67e36d460a14a4fe078d1cbbaf3190143d
Muhamad Krisnandi <mkrisnandi08@gmail.com>
2024-01-27 13:54:46 +0700

    challenge 1/366

--- 1-reduce-but-grow.js
+++ 1-reduce-but-grow.js
@@ -2,6 +2,16 @@
 // const grow = (x) => x.reduce((acc, curr) => acc * curr);
 
 // 2 Solution
-const grow=x=>eval(x.join("*"))
+// const grow=x=>eval(x.join("*"))
 
-console.log(grow([2,2,2,2,2, 2]))
+// 3 Solution
+const grow = x => {
+    let res = 1;
+    for (let i = 0; i < x.length; i++) {
+        res *= x[i]
+    }
+
+    return res
+}
+
+console.log(grow([2,2,2,2,2,2]))
